f2b66f5e293168443fb34a21487b59f7da9b0c87
zengtao227 <84569875+zengtao227@users.noreply.github.com>
2026-08-04 11:34:17 +0200

    ci: simplify strict cross-platform lint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
-  extends: "universe/native",
-  ignorePatterns: [
-    "node_modules/",
-    "dist/",
-    "release/",
-    "android/",
-    "ios/",
-    ".expo/",
-    ".eas/",
-  ],
-  env: { browser: true, node: true, es2021: true },
-  globals: { localStorage: "readonly" },
+  root: true,
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  globals: {
+    __DEV__: "readonly",
+  },
+  ignorePatterns: ["node_modules/", "dist/", "release/", "android/", "ios/", ".expo/", ".eas/"],
+  rules: {
+    "no-unused-vars": ["error", { "argsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }],
+    "no-undef": "error",
+  },
 };
